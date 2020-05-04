f2b765c7fa75d58f7c36857949b0c3f8be8c7ef2
Bhuvidha Tomar <bhuvidha.tomar@gmail.com>
2020-05-04 12:04:55 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+def home(request):
+    return render(request, 'a.html', {'titles':'DJANGO', 'link':'http://127.0.0.1:8000/'})
+
+def profile(request):
+    return render(request, 'a.html', {'titles':'PROFILE PAGE', 'link':'http://127.0.0.1:8000/'})
+def expression(request):
+    a=int(request.GET['text1'])
+    b=int(request.GET['text2'])
+    c=a+2*b
+    return render(request, 'output.html', {'result':c})
+
+
